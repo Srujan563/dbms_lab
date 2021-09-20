b1952115c68dd71ed31e99f4a69a9d10e2d8b1d6
Srujan Gundapaneni <71220310+Srujan578@users.noreply.github.com>
2021-09-20 23:42:48 +0530

    Add files via upload

---
+++ lab_3/dbhandler/sqlhandler.py
@@ -0,0 +1,28 @@
+import mysql.connector
+db = mysql.connector.connect(user="root", passwd='pwd', db="UserDB")
+
+cursor = db.cursor()
+
+
+class Mysqlhandler:
+    def __init__(self):
+        pass
+    def add_user(self,v1,v2,v3):
+        query="INSERT INTO user_data values('{}',{},'{}')".format(v1,v2,v3)
+        cursor.execute(query)
+        db.commit()
+    def update_user(self,name,dob,pno):
+        query=("update user_data set name='{}',dob='{}' where phno={}".format(name,dob,pno))
+        cursor.execute(query)
+        db.commit()
+       
+    def delete_user(self,pno):
+        query=("Delete from user_data  where pno='{}'".format(pno))
+        cursor.execute(query)
+        db.commit()
+       
+    def display_user(self,value):
+        query=("select * from user_data where name='{}'".format(value))
+        cursor.execute(query)
+
+
